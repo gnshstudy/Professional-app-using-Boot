47f2e110a52346082e91670bc5e767149de788d6
gnshstudy <gnshstudy@gmail.com>
2019-02-09 12:47:46 +0530

    2328:10th feb commit

--- BootJPADemo/src/main/java/com/example/JPADemo.java
+++ BootJPADemo/src/main/java/com/example/JPADemo.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JPADemo {
 
 	public static void main(String[] args) {
+		System.out.println("Hi");
 		SpringApplication.run(JPADemo.class, args);
 	}
 }
--- BootJPADemo/src/main/java/com/example/repository/UserRepository.java
+++ BootJPADemo/src/main/java/com/example/repository/UserRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.modal.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+	
 
 }
